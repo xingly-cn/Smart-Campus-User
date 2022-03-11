164c9b8e8855f898478f6477d133970647bff0bd
肖念昕 <sugar_tech@outlook.com>
2022-03-11 13:37:38 +0800

    框架搭建

--- tet/src/App.vue
+++ tet/src/App.vue
@@ -1,36 +1,74 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If ViewUI is successfully added to this project, you'll see an
-        <code v-text="'<Button>'"></code>
-        below
-      </p>
-      <Button type="primary">Button</Button>
+    <div class="header">
+      <div class="logo">
+        <h1>千里寻物</h1>
+      </div>
+      <Menu
+        mode="horizontal"
+        theme="light"
+        active-name="1"
+        @on-select="onSelect"
+      >
+        <MenuItem v-for="m in menu" :name="m.name" :key="m.path">
+          <Icon type="ios-paper" />
+          {{ m.meta.title }}
+        </MenuItem>
+      </Menu>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="body">
+      <router-view></router-view>
+    </div>
+    <footer class="footer">
+      COPYRIGHT © 2021 – 2022 FANGTANG. ALL RIGHTS RESERVED.
+方糖软件 版权所有
+    </footer>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  computed: {
+    menu() {
+      return this.$router.options.routes;
+    },
+  },
+  methods: {
+    onSelect(name) {
+      this.$router.push({
+        name,
+      });
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.header {
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 30px;
+  align-items: center;
+  box-shadow: 0 0 10px #ccc;
+}
+.logo {
+  
+}
+
+.footer {
+  position: fixed;
+  width: 100%;
+  display: block;
+  height: 50px;
+  line-height: 50px;
+  bottom: 0;
+  left: 0;
+  right: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.body {
+  text-align: center;
 }
 </style>
