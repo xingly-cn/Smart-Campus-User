d97c85295177640e42a56758a575f5ebb5913922
肖念昕 <sugar_tech@outlook.com>
2022-03-11 12:55:40 +0800

    init

---
+++ tet/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      title: '首页'
+    }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue'),
+    meta: {
+      title: '关于'
+    }
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
